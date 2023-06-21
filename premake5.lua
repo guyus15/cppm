0227adf45b441fcedaebfa9b2ffab1d86822c631
Guy Chamberlain-Webber <chamberlainwebberj@gmail.com>
2023-06-21 21:04:33 +0100

    Updated premake5.lua

--- premake5.lua
+++ premake5.lua
@@ -1,4 +1,4 @@
-workspace "ppm"
+workspace "cppm"
     configurations { "Debug", "Release" }
 
 project "cppm"
@@ -9,22 +9,6 @@ project "cppm"
     objdir "build/obj/%{cfg.buildcfg}"
     includedirs { "include", "src" }
     files { "src/**.cpp", "src/**.h" }
-
-    filter "configurations:Debug"
-        symbols "On"
-
-    filter "configurations:Release"
-        optimize "On"
-
-project "cppm-sandbox"
-    kind "ConsoleApp"
-    language "c++"
-    cppdialect "c++17"
-    targetdir "sandbox/bin/%{cfg.buildcfg}"
-    objdir "sandbox/obj/%{cfg.buildcfg}"
-    includedirs { "include" }
-    files { "sandbox_src/**.cpp", "sandbox_src/**.h" }
-    links { "cppm" }
 
     filter "configurations:Debug"
         symbols "On"
